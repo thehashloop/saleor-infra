6d339a585bf412cf9d802e667982111490ac4489
Pramud Bommakanti <pramud@outlook.com>
2025-03-29 17:16:22 +0000

    Fix: Store terraform state in spaces

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,8 @@ terraform {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
+    skip_get_ec2_platforms      = true         
+    skip_requesting_account_id  = true         
     use_path_style              = true         # Needed for non-AWS S3 implementations
   }
   
